cde7853175a2961e0d38072bae1f2d3bc55bf094
Dezmils-Tech-Company <dez.ezra05@gmail.com>
2026-02-06 19:11:18 +0300

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3021751845"
+    serviceWorkerVersion: "3844104337"
   }
 });
